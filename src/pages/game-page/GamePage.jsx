12fdfe21c5cdece60f8d3e89fa0505da369d10d3
TejasKedare <tejaskedare2@gmail.com>
2025-01-01 15:27:48 +0530

    added player,enemy scoreboard, game page , and starts for background for game page

---
+++ src/pages/game-page/GamePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./GamePage.css"
+
+function GamePage() {
+  return (
+    <div>
+        
+    <div class="bg-animation">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+    </div>
+    </div>
+  )
+}
+
+export default GamePage
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,24 @@
-import React, { lazy, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Footer from '../layout/Footer';
-import Header from '../layout/Header';
+import React, { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "../layout/Footer";
+import Header from "../layout/Header";
+import Scoreboard from "../pages/Scoreboard";
 
 // Lazy load components
-const Home = lazy(() => import('../pages/Home'));
+const Home = lazy(() => import("../pages/Home"));
+const GamePage = lazy(() => import("../pages/game-page/GamePage"));
 
 const AppRouter = () => {
   return (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Header /> 
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-          <Footer /> 
-        </Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/space" element={<GamePage />} />
+        <Route path="/score-board" element={<Scoreboard />} />
+      </Routes>
+      <Footer />
+    </Suspense>
   );
 };
 
